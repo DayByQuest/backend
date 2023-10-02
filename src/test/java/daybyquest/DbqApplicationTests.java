a798835923154997d40ad740e7138aa6380ad06e
DH_Choi <58378676+vectorch9@users.noreply.github.com>
2023-10-02 23:12:57 +0900

    [Chore] 개발 환경을 세팅한다. (#2)
    chore: 개발 환경을 세팅한다

---
+++ src/test/java/daybyquest/DbqApplicationTests.java
@@ -0,0 +1,13 @@
+package daybyquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
