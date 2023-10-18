aecca5b5c4860cf44fb759aba2d8bb64cd14bd5e
DH CHOI <bluetifulc@gmail.com>
2023-10-18 16:14:39 +0900

    feature: 내 프로필 조회 구현

---
+++ src/main/java/daybyquest/user/dto/response/MyProfileResponse.java
@@ -0,0 +1,37 @@
+package daybyquest.user.dto.response;
+
+import daybyquest.user.domain.Profile;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class MyProfileResponse {
+
+    private String username;
+
+    private String name;
+
+    private String imageUrl;
+
+    private Long postCount;
+
+    private Long followingCount;
+
+    private Long followerCount;
+
+    private MyProfileResponse(final String username, final String name, final String imageUrl, final Long postCount,
+        final Long followingCount, final Long followerCount) {
+        this.username = username;
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.postCount = postCount;
+        this.followingCount = followingCount;
+        this.followerCount = followerCount;
+    }
+
+    public static MyProfileResponse of(final Profile profile) {
+        return new MyProfileResponse(profile.getUsername(), profile.getName(), profile.getImageUrl(),
+            profile.getPostCount(), profile.getFollowingCount(), profile.getFollowerCount());
+    }
+}
