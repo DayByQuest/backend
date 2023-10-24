d55d0435cf47e04e83397cf4c2d20d0d25614b17
DH CHOI <bluetifulc@gmail.com>
2023-10-24 13:41:13 +0900

    feature: NoOffset 구현을 위한 VO 클래스, 유틸 추가

--- src/main/java/daybyquest/global/error/exception/BadRequestException.java
+++ src/main/java/daybyquest/global/error/exception/BadRequestException.java
@@ -3,7 +3,11 @@ package daybyquest.global.error.exception;
 import daybyquest.global.error.ExceptionCode;
 import java.util.List;
 
-public class BadRequestException extends CustomException{
+public class BadRequestException extends CustomException {
+
+    public BadRequestException() {
+        super(ExceptionCode.INVALID_REQUEST);
+    }
 
     public BadRequestException(ExceptionCode exceptionCode, List<String> fields) {
         super(exceptionCode, fields);
---
+++ src/main/java/daybyquest/global/query/LongIdList.java
@@ -0,0 +1,18 @@
+package daybyquest.global.query;
+
+import java.util.List;
+import lombok.Getter;
+
+@Getter
+public class LongIdList {
+
+    private final List<Long> ids;
+
+    public LongIdList(final List<Long> ids) {
+        this.ids = ids;
+    }
+
+    public Long getLastId() {
+        return ids.get(ids.size() - 1);
+    }
+}
