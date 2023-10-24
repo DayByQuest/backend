c06cf82a41fdcb6697bf054cdb0a1246f63544aa
DH CHOI <bluetifulc@gmail.com>
2023-10-24 14:03:25 +0900

    feature: ProfileService 도메인 서비스 작성

--- src/main/java/daybyquest/user/query/ProfileDaoQuerydslImpl.java
+++ src/main/java/daybyquest/user/query/ProfileDaoQuerydslImpl.java
@@ -5,11 +5,14 @@ import static daybyquest.relation.domain.QBlock.block;
 import static daybyquest.relation.domain.QFollow.follow;
 import static daybyquest.user.domain.QUser.user;
 
+import com.querydsl.core.types.ConstructorExpression;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import daybyquest.global.error.exception.NotExistUserException;
 import daybyquest.user.domain.Profile;
+import java.util.Collection;
+import java.util.List;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -24,21 +27,7 @@ public class ProfileDaoQuerydslImpl implements ProfileDao {
     @Override
     public Profile getByUsername(final Long userId, final String username) {
         final Profile profile = factory
-                .select(Projections.constructor(Profile.class,
-                        user.id,
-                        user.username,
-                        user.name,
-                        user.image.imageIdentifier,
-                        JPAExpressions.select(post.count())
-                                .from(post)
-                                .where(post.userId.eq(userId)),
-                        JPAExpressions.selectFrom(follow)
-                                .where(follow.userId.eq(userId).and(follow.targetId.eq(user.id)))
-                                .exists(),
-                        JPAExpressions.selectFrom(block)
-                                .where(block.userId.eq(userId).and(block.targetId.eq(user.id)))
-                                .exists()
-                ))
+                .select(profileProjection(userId))
                 .from(user)
                 .where(user.username.eq(username))
                 .fetchOne();
@@ -48,6 +37,23 @@ public class ProfileDaoQuerydslImpl implements ProfileDao {
         return profile;
     }
 
+    private ConstructorExpression<Profile> profileProjection(final Long userId) {
+        return Projections.constructor(Profile.class,
+                user.id,
+                user.username,
+                user.name,
+                user.image.imageIdentifier,
+                JPAExpressions.select(post.count())
+                        .from(post)
+                        .where(post.userId.eq(userId)),
+                JPAExpressions.selectFrom(follow)
+                        .where(follow.userId.eq(userId).and(follow.targetId.eq(user.id)))
+                        .exists(),
+                JPAExpressions.selectFrom(block)
+                        .where(block.userId.eq(userId).and(block.targetId.eq(user.id)))
+                        .exists()
+        );
+    }
 
     @Override
     public Profile getMine(final Long userId) {
@@ -74,5 +80,14 @@ public class ProfileDaoQuerydslImpl implements ProfileDao {
             throw new NotExistUserException();
         }
         return profile;
+    }
+
+    @Override
+    public List<Profile> findAllByUserIdIn(final Long userId, final Collection<Long> targetIds) {
+        return factory
+                .select(profileProjection(userId))
+                .from(user)
+                .where(user.id.in(targetIds))
+                .fetch();
     }
 }
