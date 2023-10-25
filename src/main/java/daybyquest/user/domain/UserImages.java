6c6c6ac6c0c1c2fdea1b2cd35418d4d18c5444c7
DH CHOI <bluetifulc@gmail.com>
2023-10-25 13:13:16 +0900

    style: UserImages 개행 제거

--- src/main/java/daybyquest/user/domain/UserImages.java
+++ src/main/java/daybyquest/user/domain/UserImages.java
@@ -5,8 +5,7 @@ import java.io.InputStream;
 public interface UserImages {
 
     void upload(final String identifier, final InputStream image);
-
-
+    
     void remove(final String identifier);
 
     String getPublicUrl(final String identifier);
