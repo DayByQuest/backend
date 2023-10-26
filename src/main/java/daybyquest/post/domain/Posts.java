4d5e82e58a0366988b8e675e126e3f4350989fc3
DH CHOI <bluetifulc@gmail.com>
2023-10-27 00:46:02 +0900

    refactor: Posts에서 Users를 주입받아 검증 로직을 실행하도록 리팩토링

--- src/main/java/daybyquest/post/domain/Posts.java
+++ src/main/java/daybyquest/post/domain/Posts.java
@@ -1,6 +1,7 @@
 package daybyquest.post.domain;
 
 import daybyquest.global.error.exception.NotExistPostException;
+import daybyquest.user.domain.Users;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,15 +9,25 @@ public class Posts {
 
     private final PostRepository postRepository;
 
-    Posts(final PostRepository postRepository) {
+    private final Users users;
+
+    Posts(final PostRepository postRepository, final Users users) {
         this.postRepository = postRepository;
+        this.users = users;
     }
 
     public Long save(final Post post) {
+        users.validateExistentById(post.getUserId());
         return postRepository.save(post).getId();
     }
 
     public Post getById(final Long id) {
         return postRepository.findById(id).orElseThrow(NotExistPostException::new);
+    }
+
+    public void validateExistentById(final Long postId) {
+        if (!postRepository.existsById(postId)) {
+            throw new NotExistPostException();
+        }
     }
 }
