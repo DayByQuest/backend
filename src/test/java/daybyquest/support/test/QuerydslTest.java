877fc6c32de69096e4db82e354c7922c21a76490
DH CHOI <bluetifulc@gmail.com>
2023-10-30 14:30:23 +0900

    test: QuerydslTest의 Import 어노테이션 수정

--- src/test/java/daybyquest/support/test/QuerydslTest.java
+++ src/test/java/daybyquest/support/test/QuerydslTest.java
@@ -1,14 +1,11 @@
 package daybyquest.support.test;
 
-import daybyquest.comment.domain.Comments;
 import daybyquest.global.config.JpaConfig;
-import daybyquest.post.domain.Posts;
-import daybyquest.user.domain.Users;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
 @DataJpaTest
-@Import({JpaConfig.class, Users.class, Posts.class, Comments.class})
+@Import({JpaConfig.class})
 public abstract class QuerydslTest {
 
 }
