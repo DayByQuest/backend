9bd29dc427c716c456829018a3924b740e452c9a
DH CHOI <bluetifulc@gmail.com>
2023-10-31 00:27:09 +0900

    fix: 복합 키 클래스에 equals()와 hashCode()를 추가한다

--- src/main/java/daybyquest/dislike/domain/PostDislikeId.java
+++ src/main/java/daybyquest/dislike/domain/PostDislikeId.java
@@ -2,9 +2,11 @@ package daybyquest.dislike.domain;
 
 import java.io.Serializable;
 import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode
 public class PostDislikeId implements Serializable {
 
     private Long userId;
--- src/main/java/daybyquest/group/domain/GroupUserId.java
+++ src/main/java/daybyquest/group/domain/GroupUserId.java
@@ -2,9 +2,11 @@ package daybyquest.group.domain;
 
 import java.io.Serializable;
 import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode
 public class GroupUserId implements Serializable {
 
     private Long group;
