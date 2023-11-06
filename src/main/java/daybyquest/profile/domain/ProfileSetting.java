463fd4df2e6cab07337609145480b7c9f2fc0da6
vectorch <bluetifulc@gmail.com>
2023-11-07 00:38:05 +0900

    refactor: ProfileSetting에 검증을 추가한다

--- src/main/java/daybyquest/profile/domain/ProfileSetting.java
+++ src/main/java/daybyquest/profile/domain/ProfileSetting.java
@@ -1,12 +1,14 @@
 package daybyquest.profile.domain;
 
+import static daybyquest.global.error.ExceptionCode.EXCEED_BADGE;
 import static lombok.AccessLevel.PROTECTED;
 
+import daybyquest.global.error.exception.InvalidDomainException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Convert;
-import jakarta.persistence.Embeddable;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import java.util.Collections;
 import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,6 +18,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = PROTECTED)
 public class ProfileSetting {
 
+    private static final int MAX_BADGE_LIST_SIZE = 10;
+
     @Id
     private Long userId;
 
@@ -23,4 +27,23 @@ public class ProfileSetting {
     @Convert(converter = ProfileBadgeListConverter.class)
     private List<Long> badgeList;
 
+    public ProfileSetting(final Long userId) {
+        this.userId = userId;
+        this.badgeList = Collections.emptyList();
+    }
+
+    public void updateBadgeList(final List<Long> badgeList) {
+        if (badgeList == null) {
+            this.badgeList = Collections.emptyList();
+            return;
+        }
+        this.badgeList = badgeList;
+        validateBadgeList();
+    }
+
+    private void validateBadgeList() {
+        if (badgeList.size() > MAX_BADGE_LIST_SIZE) {
+            throw new InvalidDomainException(EXCEED_BADGE);
+        }
+    }
 }
