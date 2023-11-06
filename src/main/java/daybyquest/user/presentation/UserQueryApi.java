c90fdfba068c1c9d69d10f1f174235dc1b1ef595
vectorch <bluetifulc@gmail.com>
2023-11-06 14:04:07 +0900

    feature: 사용자 이름 중복 검사를 구현한다

--- src/main/java/daybyquest/user/presentation/UserQueryApi.java
+++ src/main/java/daybyquest/user/presentation/UserQueryApi.java
@@ -2,6 +2,7 @@ package daybyquest.user.presentation;
 
 import daybyquest.auth.Authorization;
 import daybyquest.auth.domain.AccessUser;
+import daybyquest.user.application.CheckUsernameService;
 import daybyquest.user.application.GetMyProfileService;
 import daybyquest.user.application.GetProfileByUsernameService;
 import daybyquest.user.dto.response.MyProfileResponse;
@@ -18,10 +19,13 @@ public class UserQueryApi {
 
     private final GetMyProfileService getMyProfileService;
 
+    private final CheckUsernameService checkUsernameService;
+
     public UserQueryApi(final GetProfileByUsernameService getProfileByUsernameService,
-            final GetMyProfileService getMyProfileService) {
+            final GetMyProfileService getMyProfileService, final CheckUsernameService checkUsernameService) {
         this.getProfileByUsernameService = getProfileByUsernameService;
         this.getMyProfileService = getMyProfileService;
+        this.checkUsernameService = checkUsernameService;
     }
 
     @GetMapping("/profile/{username}")
@@ -37,5 +41,11 @@ public class UserQueryApi {
     public ResponseEntity<MyProfileResponse> getMyProfile(final AccessUser accessUser) {
         final MyProfileResponse response = getMyProfileService.invoke(accessUser.getId());
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/profile/{username}/check")
+    public ResponseEntity<Void> checkUsername(@PathVariable final String username) {
+        checkUsernameService.invoke(username);
+        return ResponseEntity.ok().build();
     }
 }
