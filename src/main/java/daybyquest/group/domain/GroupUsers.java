013b64f4d7ddd71dcd5e099600b8e429b5ab3ac8
vectorch <bluetifulc@gmail.com>
2023-11-13 20:22:59 +0900

    feature: 그룹 매니저 검증을 구현한다

--- src/main/java/daybyquest/group/domain/GroupUsers.java
+++ src/main/java/daybyquest/group/domain/GroupUsers.java
@@ -1,6 +1,7 @@
 package daybyquest.group.domain;
 
 import static daybyquest.global.error.ExceptionCode.NOT_DELETABLE_GROUP_USER;
+import static daybyquest.global.error.ExceptionCode.NOT_GROUP_MANAGER;
 
 import daybyquest.global.error.exception.InvalidDomainException;
 import daybyquest.global.error.exception.NotExistGroupUserException;
@@ -18,6 +19,13 @@ public class GroupUsers {
     public GroupUser getByUserIdAndGroupId(final Long userId, final Long groupId) {
         return groupUserRepository.findByUserIdAndGroupId(userId, groupId)
                 .orElseThrow(NotExistGroupUserException::new);
+    }
+
+    public void validateGroupManager(final Long userId, final Long groupId) {
+        final GroupUser groupUser = getByUserIdAndGroupId(userId, groupId);
+        if (!groupUser.isManager()) {
+            throw new InvalidDomainException(NOT_GROUP_MANAGER);
+        }
     }
 
     public void delete(final GroupUser groupUser) {
