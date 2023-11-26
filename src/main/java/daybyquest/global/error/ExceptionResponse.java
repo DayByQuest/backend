a9b1875824d43df6960ad9c0d970b0cd2a7646b0
vectorch <bluetifulc@gmail.com>
2023-11-27 00:13:19 +0900

    refactor: ExceptionResponse를 레코드로 전환한다

--- src/main/java/daybyquest/global/error/ExceptionResponse.java
+++ src/main/java/daybyquest/global/error/ExceptionResponse.java
@@ -3,25 +3,15 @@ package daybyquest.global.error;
 import daybyquest.global.error.exception.CustomException;
 import java.util.Collections;
 import java.util.List;
-import lombok.Getter;
 
-@Getter
-public class ExceptionResponse {
-
-    private final String code;
-
-    private final String message;
-
-    private final List<String> fields;
-
-    public ExceptionResponse(String code, String message, List<String> fields) {
-        this.code = code;
-        this.message = message;
-        this.fields = fields;
-    }
+public record ExceptionResponse(String code, String message, List<String> fields) {
 
     public static ExceptionResponse of(CustomException e) {
         return new ExceptionResponse(e.getCode(), e.getMessage(), e.getFields());
+    }
+
+    public static ExceptionResponse of(ExceptionCode errorCode, String field) {
+        return new ExceptionResponse(errorCode.getCode(), errorCode.getMessage(), List.of(field));
     }
 
     public static ExceptionResponse of(ExceptionCode errorCode, List<String> fields) {
