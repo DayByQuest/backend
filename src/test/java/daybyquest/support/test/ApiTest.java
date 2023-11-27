dc0186933c4bba3563764b909a457426f953a49d
vectorch <bluetifulc@gmail.com>
2023-11-27 13:44:01 +0900

    test: ApiTest에 멀티파트 관련 메서드, 사용자 및 어드민 인증 헤더를 추가한다

--- src/test/java/daybyquest/support/test/ApiTest.java
+++ src/test/java/daybyquest/support/test/ApiTest.java
@@ -1,6 +1,8 @@
 package daybyquest.support.test;
 
 import static daybyquest.support.fixture.UserFixtures.ALICE;
+import static daybyquest.support.fixture.UserFixtures.BOB;
+import static daybyquest.user.domain.UserState.ADMIN;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
@@ -10,15 +12,20 @@ import static org.springframework.restdocs.operation.preprocess.Preprocessors.pr
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import daybyquest.global.config.WebMvcConfig;
+import daybyquest.user.domain.User;
 import daybyquest.user.domain.Users;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.restdocs.request.RequestPartsSnippet;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultHandler;
 
@@ -26,7 +33,15 @@ import org.springframework.test.web.servlet.ResultHandler;
 @AutoConfigureRestDocs
 public abstract class ApiTest {
 
+    protected final String 인증_헤더_이름 = "Authorization";
+
+    protected final String 사용자_인증_헤더 = "UserId 1";
+
+    protected final String 어드민_인증_헤더 = "UserId 2";
+
     protected final Long 로그인_ID = 1L;
+
+    protected final Long 어드민_ID = 2L;
 
     @MockBean
     private Users users;
@@ -39,7 +54,21 @@ public abstract class ApiTest {
 
     @BeforeEach
     void setUpAuthentication() {
-        given(users.getById(1L)).willReturn(ALICE.생성(로그인_ID));
+        given(users.getById(로그인_ID)).willReturn(ALICE.생성(로그인_ID));
+        final User admin = BOB.생성(로그인_ID);
+        ReflectionTestUtils.setField(admin, "state", ADMIN);
+        given(users.getById(어드민_ID)).willReturn(admin);
+    }
+
+    protected MockMultipartFile 사진을_전송한다(final String name) {
+        return new MockMultipartFile(name, "image.png",
+                MediaType.MULTIPART_FORM_DATA_VALUE, "file content".getBytes());
+    }
+
+    protected MockMultipartFile 멀티파트_JSON을_전송한다(final String name, final Object request)
+            throws JsonProcessingException {
+        return new MockMultipartFile(name, "request",
+                MediaType.APPLICATION_JSON_VALUE, objectMapper.writeValueAsBytes(request));
     }
 
     protected ResultHandler 문서화한다(final String identifier) {
