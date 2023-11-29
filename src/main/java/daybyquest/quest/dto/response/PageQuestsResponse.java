71fa88648dcd54468d63f8b126d5fbd14ce9ef14
vectorch <58378676+vectorch9@users.noreply.github.com>
2023-11-29 20:24:51 +0900

    [Fix] 퀘스트 검색 시 응답의 users를 quests로 변경한다 (#148)
    * fix: PageQuestsResponse내 users를 quests로 고친다
    
    * chore: index.adoc을 수정한다

--- src/main/java/daybyquest/quest/dto/response/PageQuestsResponse.java
+++ src/main/java/daybyquest/quest/dto/response/PageQuestsResponse.java
@@ -2,6 +2,6 @@ package daybyquest.quest.dto.response;
 
 import java.util.List;
 
-public record PageQuestsResponse(List<QuestResponse> users, Long lastId) {
+public record PageQuestsResponse(List<QuestResponse> quests, Long lastId) {
 
 }
