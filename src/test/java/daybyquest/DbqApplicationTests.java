55d06ef67b9b25b936c251d553c2b8dca892bb82
vectorch <bluetifulc@gmail.com>
2023-12-09 15:26:43 +0900

    style: 탭 인덴트를 스페이스로 변경한다

--- src/test/java/daybyquest/DbqApplicationTests.java
+++ src/test/java/daybyquest/DbqApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DbqApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
