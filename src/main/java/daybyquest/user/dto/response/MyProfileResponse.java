73b91d6f0e56c9666957efa642ac6218e6c6c767
vectorch <bluetifulc@gmail.com>
2023-12-15 13:02:34 +0900

    refactor: MyProfileResponse를 레코드로 변경한다

--- src/main/java/daybyquest/user/dto/response/MyProfileResponse.java
+++ src/main/java/daybyquest/user/dto/response/MyProfileResponse.java
@@ -1,32 +1,9 @@
 package daybyquest.user.dto.response;
 
 import daybyquest.user.query.Profile;
-import lombok.Getter;
 
-@Getter
-public class MyProfileResponse {
-
-    private final String username;
-
-    private final String name;
-
-    private final String imageIdentifier;
-
-    private final Long postCount;
-
-    private final Long followingCount;
-
-    private final Long followerCount;
-
-    private MyProfileResponse(final String username, final String name, final String imageIdentifier,
-            final Long postCount, final Long followingCount, final Long followerCount) {
-        this.username = username;
-        this.name = name;
-        this.imageIdentifier = imageIdentifier;
-        this.postCount = postCount;
-        this.followingCount = followingCount;
-        this.followerCount = followerCount;
-    }
+public record MyProfileResponse(String username, String name, String imageIdentifier, Long postCount,
+                                Long followingCount, Long followerCount) {
 
     public static MyProfileResponse of(final Profile profile) {
         return new MyProfileResponse(profile.getUsername(), profile.getName(), profile.getImageIdentifier(),
