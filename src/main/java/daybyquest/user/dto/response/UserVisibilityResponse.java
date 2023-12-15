af0fea4a744c6d8c521a190e8444795043819ee9
vectorch <bluetifulc@gmail.com>
2023-12-15 14:34:32 +0900

    refactor: UserVisibilityResponse를 레코드화 한다

--- src/main/java/daybyquest/user/dto/response/UserVisibilityResponse.java
+++ src/main/java/daybyquest/user/dto/response/UserVisibilityResponse.java
@@ -1,16 +1,8 @@
 package daybyquest.user.dto.response;
 
 import daybyquest.user.domain.User;
-import lombok.Getter;
 
-@Getter
-public class UserVisibilityResponse {
-
-    private final String visibility;
-
-    private UserVisibilityResponse(final String visibility) {
-        this.visibility = visibility;
-    }
+public record UserVisibilityResponse(String visibility) {
 
     public static UserVisibilityResponse of(final User user) {
         return new UserVisibilityResponse(user.getVisibility().toString());
