144d7c300669cf32cc87e7b04a96278e02e72ac3
vectorch <bluetifulc@gmail.com>
2023-12-16 12:59:50 +0900

    test: GetTrackerService 테스트를 수정한다

--- src/test/java/daybyquest/post/application/GetTrackerServiceTest.java
+++ src/test/java/daybyquest/post/application/GetTrackerServiceTest.java
@@ -1,51 +1,47 @@
 package daybyquest.post.application;
 
+import static daybyquest.support.fixture.PostFixtures.POST_1;
 import static daybyquest.support.fixture.UserFixtures.ALICE;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 
+import daybyquest.post.domain.Posts;
 import daybyquest.post.dto.response.TrackerResponse;
-import daybyquest.post.query.PostDao;
-import daybyquest.post.query.SimplePostData;
-import daybyquest.user.domain.Users;
+import daybyquest.support.test.ServiceTest;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 
-@ExtendWith(MockitoExtension.class)
-public class GetTrackerServiceTest {
+public class GetTrackerServiceTest extends ServiceTest {
 
-    private final Users users = BDDMockito.mock(Users.class);
+    @Autowired
+    private GetTrackerService getTrackerService;
 
-    private final PostDao postDao = BDDMockito.mock(PostDao.class);
-
-    private final GetTrackerService getTrackerService = new GetTrackerService(users, postDao, 5L);
+    @Autowired
+    private Posts posts;
 
     @Test
     void 트래커를_조회한다() {
         // given
-        final Long loginId = 1L;
-        final Long aliceId = 2L;
+        final Long id = ALICE를_저장한다();
         final LocalDateTime now = LocalDateTime.now();
-        final List<SimplePostData> simplePostData = List.of(
-                new SimplePostData(1L, now),
-                new SimplePostData(2L, now.minusDays(1)),
-                new SimplePostData(3L, now.minusDays(1)),
-                new SimplePostData(4L, now.minusDays(3)),
-                new SimplePostData(4L, now.minusDays(4))
-        );
+
+        given(dataTimeProvider.getNow()).willReturn(Optional.of(now));
+        posts.save(POST_1.생성(id));
+        given(dataTimeProvider.getNow()).willReturn(Optional.of(now.minusDays(1)));
+        posts.save(POST_1.생성(id));
+        posts.save(POST_1.생성(id));
+        given(dataTimeProvider.getNow()).willReturn(Optional.of(now.minusDays(3)));
+        posts.save(POST_1.생성(id));
+        given(dataTimeProvider.getNow()).willReturn(Optional.of(now.minusDays(4)));
+        posts.save(POST_1.생성(id));
+
         final List<Long> expected = List.of(1L, 2L, 0L, 1L, 1L);
 
-        given(users.getUserIdByUsername(ALICE.username)).willReturn(aliceId);
-        given(postDao.findAllBySuccessAndUploadedAtAfter(eq(aliceId), any())).willReturn(simplePostData);
-
         // when
-        final TrackerResponse response = getTrackerService.invoke(loginId, ALICE.username);
+        final TrackerResponse response = getTrackerService.invoke(id, ALICE.username);
         final List<Long> tracker = response.tracker();
 
         // then
